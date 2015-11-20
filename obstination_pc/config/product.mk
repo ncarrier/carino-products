d23db8efa35c7936b103e4eec6a0d7041aa42e40
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-20 05:38:34 +0100

    draft creation of the pc variant

---
+++ obstination_pc/config/product.mk
@@ -0,0 +1,43 @@
+###############################################################################
+## @file product.mk
+##
+## Base alchemy configuration for the obstination vehicle
+###############################################################################
+
+OBSTINATION_PC_CONFIG_DIR := $(call my-dir)
+COMMON_DIR := $(OBSTINATION_PC_CONFIG_DIR)/../../common
+
+TARGET_OS_FLAVOUR := native-chroot
+TARGET_LIBC := native
+# Detect the host architecture to set the right TARGET_ARCH.
+ifeq ($(shell getconf LONG_BIT),32)
+    TARGET_ARCH := x86
+else
+    TARGET_ARCH := x64
+endif
+TARGET_CPU :=
+
+# Add Base skeleton
+TARGET_SKEL_DIRS += $(COMMON_DIR)/skel
+TARGET_SKEL_DIRS += $(OBSTINATION_PC_CONFIG_DIR)/../skel
+$(warning $(TARGET_SKEL_DIRS))
+# TODO understand the next two
+# Activate .alchemy.depends section generation
+TARGET_ADD_DEPENDS_SECTION := 1
+
+# Activate .alchemy.build-id section generation
+TARGET_ADD_BUILDID_SECTION := 1
+
+# the image target is used by gen_deb.sh
+TARGET_IMAGE_FORMAT := ext2
+TARGET_IMAGE_OPTIONS := --size 60M
+
+custom.busybox.config := $(COMMON_DIR)/config/busybox.config
+LINUX_CONFIG_FILE := $(OBSTINATION_PC_CONFIG_DIR)/linux.config
+
+#LINUX_DEFAULT_CONFIG_TARGET := sunxi_defconfig
+
+# TODO reenable when logwrapping will be useful
+#RELOG_FORCE_DEFAULT_OUTPUT_TO := /dev/ulog_main
+#RELOG_FORCE_DEFAULT_ERROR_TO := /dev/ulog_main
+
